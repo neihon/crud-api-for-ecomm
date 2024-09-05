fa06f8e942f76016211ff4227f3353415cebdf15
neihon <jorogeorgievxd@gmail.com>
2024-09-05 09:57:07 +0300

    format code / returns memInfo struct slice now, not specifics, to be accessed from main

--- activityMonitorGo/metricsCollector/metricsCollector.go
+++ activityMonitorGo/metricsCollector/metricsCollector.go
@@ -1,4 +1,4 @@
-package main
+package metricsCollector
 
 import (
 	"bytes"
@@ -13,6 +13,7 @@ func fetchMemInfo() (*mem.VirtualMemoryStat, error) {
 	memInfo, memErr := mem.VirtualMemory()
 	if memErr != nil {
 		return nil, fmt.Errorf("memory error: %v", memErr)
+		// change to log info
 	}
 
 	return memInfo, nil
@@ -26,22 +27,25 @@ func fetchCPUInfo(key string) (string, error) {
 	err := cmd.Run()
 	if err != nil {
 		return "", fmt.Errorf("failed to fetch %s: %v", key, err)
+		// change to log info
 	}
 
 	return strings.TrimSpace(out.String()), nil
 }
 
-func collectedData() (uint64, uint64, uint64, float64, uint64, uint64, uint64, uint64, uint64, uint64, uint64, uint64, uint64, int, int, int) {
+func collectedData() (*mem.VirtualMemoryStat, int, int, int) {
 	memInfo, memErr := fetchMemInfo()
 	if memErr != nil {
 		fmt.Println(memErr)
-		return 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+		// change to log info
+		return nil, 0, 0, 0
 	}
 
 	coreCntStr, err := fetchCPUInfo("machdep.cpu.core_count")
 	if err != nil {
 		fmt.Println("Error fetching core count:", err)
-		return 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+		// change to log info
+		return nil, 0, 0, 0
 	}
 	coreCnt, _ := strconv.Atoi(coreCntStr)
 
@@ -49,7 +53,7 @@ func collectedData() (uint64, uint64, uint64, float64, uint64, uint64, uint64, u
 	threadCntStr, err := fetchCPUInfo("machdep.cpu.thread_count")
 	if err != nil {
 		fmt.Println("Error fetching thread count:", err)
-		return 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+		return nil, 0, 0, 0
 	}
 	threadCnt, _ := strconv.Atoi(threadCntStr)
 
@@ -57,30 +61,13 @@ func collectedData() (uint64, uint64, uint64, float64, uint64, uint64, uint64, u
 	actCoresStr, err := fetchCPUInfo("hw.ncpu")
 	if err != nil {
 		fmt.Println("Error fetching active cores:", err)
-		return 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+		// change to log info
+		return nil, 0, 0, 0
 	}
 	actCores, _ := strconv.Atoi(actCoresStr)
 
-	// TODO: fix the cpu metrics
-	// for mem
-	memTotal := memInfo.Total
-	memFree := memInfo.Free
-	memAvailable := memInfo.Available //totalMem - freeMem
-	usedPercent := memInfo.UsedPercent
-	memBuffers := memInfo.Buffers
-	memActive := memInfo.Active
-	memCached := memInfo.Cached
-	memDirty := memInfo.Dirty
-	memInactive := memInfo.Inactive
-	memHighTotal := memInfo.HighTotal
-	memLowTotal := memInfo.LowTotal
-	memHighFree := memInfo.HighFree
-	memLowFree := memInfo.LowFree
+	// TODO: fix the cpu metrics -> fixed
 
-	// TOneverDO: use again -> for cpu
-	//coreCnt := machdep.cpu.core_count
-	//threadCnt := machdep.cpu.thread_count
-	//actCores := hw.ncpu
-
-	return memTotal, memFree, memAvailable, usedPercent, memBuffers, memActive, memCached, memDirty, memInactive, memHighTotal, memLowTotal, memHighFree, memLowFree, coreCnt, threadCnt, actCores
+	// returns the whole memInfo struct slice
+	return memInfo, coreCnt, threadCnt, actCores
 }
